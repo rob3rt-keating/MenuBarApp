eb281516cdce13ff14244ca5acf12639306febd3
Robert <robert@Roberts-MacBook-Air.local>
2014-12-15 12:10:14 -0700

    Initial Commit

---
+++ MenuBarApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuBarApp
+//
+//  Created by Robert on 12/15/14.
+//  Copyright (c) 2014 Robert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
